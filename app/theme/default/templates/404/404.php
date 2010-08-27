d46fb289b5cc8403d47485770b174a6f241ffe19
Luke Giuliani <luke@squareweave.com.au>
2010-08-27 01:45:49 +0000

    added 404 and statics

---
+++ app/theme/default/templates/404/404.php
@@ -0,0 +1,9 @@
+<?php $this->display( 'header' );  ?>
+
+			<div class="content">
+				<h2>Four Oh Four!</h2>
+				<p>Oh noes! I couldn't find that!</p>
+				<p>Sorry. Don't hate me. Try going back <a href='<?php EF::dest() ?>'>to the home page</a>.</p>
+			</div><!-- /content -->
+
+<?php $this->display( 'footer' );  ?>
---
+++ app/theme/default/templates/statics/woop.php
@@ -0,0 +1,9 @@
+<?php $this->display( 'header' );  ?>
+
+			<div class="content">
+				<h2>>Woop</h2>
+				
+				<p>Woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop woop.</p>
+			</div><!-- /content -->
+
+<?php $this->display( 'footer' );  ?>
