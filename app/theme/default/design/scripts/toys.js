d36ffb77a90c9ba9470827e31660517b921a2de9
Will Dayble <will@squareweave.com.au>
2011-10-05 06:43:18 +0000

    standard toys

--- app/theme/default/design/scripts/toys.js
+++ app/theme/default/design/scripts/toys.js
@@ -1,1 +1,9 @@
-// will does some fun toys in here.
+// Squareweave designers does some fun toys in here
+// They're not real javascript, they're FAKE javascripts that have no maths.
+
+jQuery(document).ready(function($) {
+  
+  // Code using $ as usual goes here, like:
+  // $('.my_class').fadeIn();
+  
+});
