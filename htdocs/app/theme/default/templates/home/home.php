5b76287ccdaa20f893d4f69184293df4072f7973
Sebastian Sibelle <sebastian.sibelle@squareweave.com.au>
2014-09-08 11:14:28 +1000

    Added home homey

--- htdocs/app/theme/default/templates/home/home.php
+++ htdocs/app/theme/default/templates/home/home.php
@@ -3,7 +3,7 @@
 	<header>
 		<div class="constrain">
 			<h1>Sebastian Sibelle</h1>
-			<h2>Engineer by nature, hipster by nurture. Dismantling human problems and mending them with the internet.</h2>
+			<h2>Creating friendly and effective solutions to human problems using the Internet.</h2>
 			<p><a href="mailto:sebastian.sibelle@gmail.com">sebastian.sibelle@gmail.com</a></p>
 		</div>
 	</header>
@@ -12,32 +12,34 @@
 		<div class="constrain">
 			<aside>
 				<ul class="contact unstyled clearfix">
-					<li><a href="http://twitter.com/sgsibelle"><i class="ss-social ss-twitter"></i> <span class="title">@sgsibelle</span></a></li>
 					<li><a href="http://au.linkedin.com/in/sebastiansibelle"><i class="ss-social ss-linkedin"></i> <span class="title">LinkedIn</span></a></li>
 					<li><a href="https://github.com/sebastiansibelle"><i class="ss-social ss-github"></i> <span class="title">Github</span></a></li>
+					<li><a href="http://twitter.com/sgsibelle"><i class="ss-social ss-twitter"></i> <span class="title">@sgsibelle</span></a></li>
 				</ul>
 			</aside>
 			<section>
+				<h2>What I'm working on now.</h2>
 				<dl class="projects">
-					<dt>Web Developer at <a href="http://squareweave.com.au">Squareweave</a><dt><dd>Technology design and implementation. Code, databases, APIs and optimisation. Documentation management and server management.</dd>
-					<dt>Member of <a href="http://capsule.vc">Capsule</a></dt><dd>Three piece band of rocking out internet and doing projects together like pals.</dd>
-					<dt>Co-Founder of <a href="http://regular.io">Regular</a></dt><dd> Automate subscription billing.</dd>
+					<dt>Freelance Web Developer<dt><dd>Technology design and implementation. I write code in Ruby, PHP and JavaScript to solve human problems. Got a problem you need solved with the internet? I can help.</dd>
+					<dt>Visionary at <a href="http://jusbchill.com">Jus B Chill</a><dt><dd> Jus B Chill is an online based label which releases techno music. We throw parties, b chill and have fun.</dd>
+					<!-- <dt> of <a href="http://capsule.vc">Capsule</a></dt><dd>Three piece band of rocking out internet and doing projects together like pals.</dd> -->
 				</ul>
 			</section>
 			<section>
 				<h2>Projects I've worked on.</h2>
 				<dl class="projects">
-					<dt>Helped build the <a href="http://promobay.org">Promobay</a><dt><dd>Building front and back end for the Promobay. Improving the user experience and choosing features from user feedback.</dd>
 					<dt>Project managed and implemented the <a href="http://climatecouncil.org.au">Climate Council website</a></dt><dd>Managed the project for the new Climate Council Website</dd>
+					<dt>Helped build the <a href="http://promobay.org">Promobay</a><dt><dd>Building front and back end for the Promobay. Improving the user experience and choosing features from user feedback.</dd>
 					<dt>Built and maintain <a href="http://roadtorefuge.org.au">Road to Refuge</a></dt><dd> Build the back-end for the road to refuge site including CMS</dd>
 					<dt>Taught <strong>Code Dependency</strong></dt><dd> Taught a group of 10 the basics of the internet and programming as part of a 6 month course</dd>
 					<dt>Built <a href="http://goatremix.com">Goat Remix Dot Com</a> in an afternoon</dt><dd> Built a site to agreggate goat remix videos on youtube so you can have goats all day long!</dd>
 				</ul>
 			</section>
 			<section>
+				<h2>My previous employment.</h2>
 				<dl class="projects">
-					<h2>Previous employment</h2>
-					<dt>Projectionist at <a href="http://rooftopcinema.com.au">Rooftop Cinema</a><dt><dd></dd>
+					<dt>Web Developer at <a href="http://squareweave.com.au">Squareweave</a><dt><dd>Technology design and implementation. Code, databases, APIs and optimisation. Documentation management and server management.</dd>
+					<dt>Projectionist at <a href="http://rooftopcinema.com.au">Rooftop Cinema</a><dt><dd>Running and screening movies at the Rooftop Cinema.</dd>
 				</ul>
 			</section>
 		</div>
