fae764cd1113a0c55902ba1e7c191a5c749fb107
Sebastian Sibelle <sebastian@chuffed.org>
2015-08-24 18:50:17 +1000

    Updated home witha  better call to action

--- htdocs/app/theme/default/templates/home/home.php
+++ htdocs/app/theme/default/templates/home/home.php
@@ -19,13 +19,12 @@
 			</aside>
 			<section>
 				<h2>What I'm working on now.</h2>
-				<p class="lead">I write code in Ruby, PHP and JavaScript to solve human problems. Got a problem you need solved with the internet? I can help.</p>
 				<dl class="projects">
 					<dt>Web Developer at <a href="https://chuffed.org">Chuffed</a><dt><dd>I'm working across the full stack and writing code in PHP (Laravel) and Javascript (AngularJS). I work on scaling the application, processing large amounts of transactions, code architecture and database design.</dd>
 					<dt>Visionary at <a href="http://jusbchill.com">Jus B Chill</a><dt><dd> Jus B Chill is an online based label which releases techno music. We throw <a href="http://jusbchill.com/coolroom">parties</a>, b chill and have fun.</dd>
 					<dt>Volunteer at <a href="http://roadtorefuge.org.au">Road to Refuge</a></dt><dd> I oversaw the initial build for the Road to Refuge website and continue to volunteer my time to get the message out there.</dd>
 					<!-- <dt> of <a href="http://capsule.vc">Capsule</a></dt><dd>Three piece band of rocking out internet and doing projects together like pals.</dd> -->
-				</ul>
+				</dl>
 			</section>
 			<section>
 				<h2>Projects I've worked on.</h2>
@@ -34,15 +33,21 @@
 					<dt>Helped build the <a href="http://promobay.org">Promobay</a><dt><dd>Building front and back end for the Promobay. Improving the user experience and choosing features from user feedback.</dd>
 					<dt>Taught <strong>Code Dependency</strong></dt><dd> Taught a group of 10 the basics of the internet and programming as part of a 6 month course</dd>
 					<!-- <dt>Built <a href="http://goatremix.com">Goat Remix Dot Com</a> in an afternoon</dt><dd> Built a site to agreggate goat remix videos on youtube so you can have goats all day long!</dd> -->
-				</ul>
+				</dl>
 			</section>
 			<section>
 				<h2>My previous employment.</h2>
 				<dl class="projects">
 					<dt>Web Developer at <a href="http://squareweave.com.au">Squareweave</a><dt><dd>Technology design and implementation. Code, databases, APIs and optimisation. Documentation management and server management.</dd>
 					<dt>Projectionist at <a href="http://rooftopcinema.com.au">Rooftop Cinema</a><dt><dd>Running and screening movies at the Rooftop Cinema.</dd>
-				</ul>
+				</dl>
 			</section>
+			<section>
+				<h2>Get in touch</h2>
+				<p class="lead">I write code in Ruby, PHP and JavaScript to solve human problems. Got a problem you need solved with the internet? I can help.</p>
+				<p><a href="mailto:sebastian.sibelle@gmail.com">sebastian.sibelle@gmail.com</a></p>
+			</section>
+
 		</div>
 	</article>
 
